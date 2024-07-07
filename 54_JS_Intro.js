6ba8f5bb3e740cdc55fbb719f3435e7da124104d
Abdullah-KhanJr <63127711+Abdullah-KhanJr@users.noreply.github.com>
2024-07-07 06:14:13 +0500

    Gap

---
+++ 54_JS_Intro.js
@@ -0,0 +1,14 @@
+// alert ("SITE NOT SAFE");
+
+        console.log("Code is runnnig")
+
+        document.title = "Node JS"
+
+        var a = prompt("Enter your age:")
+        var isTrue = confirm("This site is not safe, Confirm and procede.")
+
+        if (isTrue) {
+            console.log("Proceded")
+        } else {
+            console.log("Not proceded")
+        }
