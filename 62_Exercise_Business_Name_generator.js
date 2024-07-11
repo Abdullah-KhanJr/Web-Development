c4ae98b8b8bef3f7000e51fef9fa10477ff63c1c
Abdullah-KhanJr <63127711+Abdullah-KhanJr@users.noreply.github.com>
2024-07-12 01:11:08 +0500

    Exercise | Business Name Generator

---
+++ 62_Exercise_Business_Name_generator.js
@@ -0,0 +1,70 @@
+console.log ("Business Name Genarator")
+
+// ADJ + Name + Misc
+
+// ADJ:
+// Crazy
+// Amazing
+// Fire
+
+// Name:
+// Engine
+// Foods
+// Garments
+
+// Misc:
+// Bros
+// Limited
+// Hub
+
+
+let NameADJ = {
+    ADJ: "Crazy",
+    ADJ: "Amazing",
+    ADJ: "Fire"
+}
+
+let Name_Name = {
+    Name: "Engine",
+    Name: "Foods",
+    Name: "Garments",
+}
+
+let NameMisc = {
+    Misc: "Bros",
+    Misc: "Limited",
+    Misc: "Hub"
+}
+
+let a = Math.random(NameMisc)
+console.log(NameMisc)
+
+// let FirstName = Math.random()
+// let SecondName = Math.random()
+// let ThirdName = Math.random()
+
+// if (FirstName <= 0.33) {
+//     FirstName = "Crazy"
+// } else if (FirstName > 0.33 && FirstName <= 0.66) {
+//     FirstName = "Amazing"
+// } else {
+//     FirstName = "Fire"
+// }
+
+// if (SecondName <= 0.33) {
+//     SecondName = "Engine"
+// } else if (SecondName > 0.33 && SecondName <= 0.66) {
+//     SecondName = "Foods"
+// } else {
+//     SecondName = "Garments"
+// }
+
+// if (ThirdName > 0.66) {
+//     ThirdName = "Bros"
+// } else if (ThirdName > 0.33 && ThirdName <= 0.66) {
+//     ThirdName = "Limited"
+// } else {
+//     ThirdName = "Hub"
+// }
+
+// console.log(`Business Name: ${FirstName} ${SecondName} ${ThirdName}`)
