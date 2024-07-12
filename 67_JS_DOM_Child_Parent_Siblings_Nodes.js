fb3ba549e176478d384887eedcd889081ac0333f
Abdullah-KhanJr <63127711+Abdullah-KhanJr@users.noreply.github.com>
2024-07-13 04:10:07 +0500

    JS DOM

---
+++ 67_JS_DOM_Child_Parent_Siblings_Nodes.js
@@ -0,0 +1,14 @@
+console.log ("DOM Chlidren, Parents & Sibling Nodes")
+
+
+// CHILD NODES
+document.body.childNodes[1]
+// document.body
+// document.body.childNodes
+// document.body.firstChild //return text if there are blank spaces
+
+// to directly access the elements
+// document.body.firstElementChild
+// document.body.lastElementChild
+
+//PARENT NODES
